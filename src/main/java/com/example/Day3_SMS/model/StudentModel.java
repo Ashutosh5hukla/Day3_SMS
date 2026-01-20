a1673d2608eff5e430065c53e23284ba8ac624e1
Ashutosh Shukla <ashutosh20051978@gmail.com>
2026-01-20 12:49:27 +0530

    Added files

---
+++ src/main/java/com/example/Day3_SMS/model/StudentModel.java
@@ -0,0 +1,23 @@
+package com.example.Day3_SMS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Students")
+public class StudentModel {
+
+    @Id
+    private String id;
+
+    private String name;
+    private int age;
+    private String email;
+
+
+}
